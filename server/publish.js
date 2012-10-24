e79ae2e86b2146b7a8379f4875e7ed54f779778d
Jeremy Cerise <jcerise06@gmail.com>
2012-10-24 16:37:11 -0600

    Fixed a small typo on new collection creation

--- server/publish.js
+++ server/publish.js
@@ -1,6 +1,6 @@
 //Snippets -- {title: String,
 //             tags: [String, ...]}
-Snippets = new Meteor.collection("snippets");
+Snippets = new Meteor.Collection("snippets");
 
 Meteor.publish('snippets', function() {
   return Snippets.find();
